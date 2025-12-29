818ad4f9b0f4acb0ef1027ed3c6ec651e5b98dbc
Topdev <107178157+LydiaGS@users.noreply.github.com>
2025-12-29 22:54:23 +0100

    Update scripts.js

--- Javascripts/scripts.js
+++ Javascripts/scripts.js
@@ -1,21 +1,6 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { auth } from "./firebase.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-
-  // ===== MENU BURGER =====
-  const burger = document.getElementById("burger");
-  const sidebar = document.getElementById("sidebar");
-
-  if (burger && sidebar) {
-    burger.addEventListener("click", () => {
-      sidebar.classList.toggle("active");
-      console.log("Burger cliqué");
-    });
-  } else {
-    console.error("Burger ou sidebar introuvable");
-  }
-
   // ===== ANNEE FOOTER =====
   const year = document.getElementById("year");
   if (year) {
