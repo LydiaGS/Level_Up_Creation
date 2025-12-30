8a90c8463b531a6a9eaa59711efd9fdb8ed74bd5
Topdev <107178157+LydiaGS@users.noreply.github.com>
2025-12-30 15:03:38 +0100

    Update quiz.js

--- Javascripts/quiz.js
+++ Javascripts/quiz.js
@@ -94,25 +94,5 @@ function restartQuiz() {
 }
 
 showQuestion();
-/* devis*/
-function calculateBudget() {
-  const siteType = Number(document.getElementById("siteType").value);
-  const pages = Number(document.getElementById("pages").value);
-  const options = document.querySelectorAll(".option");
 
-  let total = siteType;
-
-  // Prix par page (au-delà de la 1ère)
-  if (pages > 1) {
-    total += (pages - 1) * 120;
-  }
-
-  options.forEach(option => {
-    if (option.checked) {
-      total += Number(option.value);
-    }
-  });
-
-  document.getElementById("total").textContent = total + " €";
-}
 
