7fd16c7d3726a98af085731a5bbf01fe8059c5f1
Topdev <107178157+LydiaGS@users.noreply.github.com>
2025-12-30 15:46:21 +0100

    Update scripts.js

--- Javascripts/scripts.js
+++ Javascripts/scripts.js
@@ -19,6 +19,23 @@ document.querySelectorAll(".toggle-password").forEach(icon => {
     input.type = input.type === "password" ? "text" : "password";
   });
 });
+/* fèche vers le haut */
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
 
 
 
